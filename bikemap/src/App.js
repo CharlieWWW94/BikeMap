d7c7883aab64fd4cccea4082b685fbc85cdb5989
Charlie Whiteside <charlie.whiteside@rocksteadymusicschool.com>
2022-11-14 11:21:25 +0000

    Add router to react application

--- bikemap/src/App.js
+++ bikemap/src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Page1 from "./pages/page1";
+import Page2 from "./pages/page2";
+import Page3 from "./pages/page3";
+import { Route, Routes } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Page1 />} />
+      <Route path="/2" element={<Page2 />} />
+      <Route path="/3" element={<Page3 />} />
+    </Routes>
   );
 }
 
---
+++ bikemap/src/pages/page3.js
@@ -0,0 +1,3 @@
+export default function Page3() {
+  return <h1>page 3</h1>;
+}
